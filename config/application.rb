e37b76a613c2143535ca71acda88c98400be850d
Tony Porteous <tony.porteous@gmail.com>
2019-01-11 14:15:02 +0000

    Wrapper for STDOUT logging

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ module Summongrid
     config.load_defaults 5.2
 
     #AREP::NOTE::REFINE ME
-    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
